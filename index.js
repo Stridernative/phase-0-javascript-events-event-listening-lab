ca1fc4d8490b10200a5448415f57fff34e5607eb
gabriel <gabecreacs@gmail.com>
2021-08-03 19:26:20 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.querySelector("#input");
+    
+  function clickAlert(){
+    alert('I was clicked!');
 }
+    input.addEventListener('click', clickAlert);
+}
+
